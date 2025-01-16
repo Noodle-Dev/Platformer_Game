de530d5785b9d1c01c844296e70f255fb386aa80
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2025-01-15 22:56:05 -0600

    Minor changes

--- assets/scripts/Player/Player_Logic.gd
+++ assets/scripts/Player/Player_Logic.gd
@@ -1,13 +1,14 @@
 extends CharacterBody2D
 
 # Constants
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const GRAVITY = 1200.0
+const SPEED = 350.0
+const JUMP_VELOCITY = -600.0
+const GRAVITY = 1300.0
 const BOUNCE_FORCE = -200.0  # Force applied when bouncing off enemies
 
 # Variables
 var is_invulnerable = false  # Temporarily avoid damage after hitting an enemy
+@onready var camera_2d = $"../Camera2D"
 
 func _physics_process(delta):
 	# Apply gravity
@@ -15,7 +16,7 @@ func _physics_process(delta):
 		velocity.y += GRAVITY * delta
 
 	# Handle movement input
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("Left", "Right")
 	velocity.x = direction * SPEED
 
 	# Flip the sprite based on movement direction
@@ -23,8 +24,20 @@ func _physics_process(delta):
 		$Sprite2D.flip_h = direction < 0
 
 	# Handle jumping
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	# Play animations based on state
+	if not is_on_floor():
+		$Sprite2D.play("Jump")
+	elif direction != 0:
+		$Sprite2D.play("Walk")
+	else:
+		$Sprite2D.play("Idle")
+
+	# Update camera position to follow the player
+	if camera_2d:
+		camera_2d.position = position
 
 	# Move the player
 	move_and_slide()
