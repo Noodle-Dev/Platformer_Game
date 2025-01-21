6c63fe9f509eab5d7bcdb0e557936c6f9aa82853
Noodle Dev <92656632+Noodle-Dev@users.noreply.github.com>
2025-01-20 20:15:07 -0600

    Revision

---
+++ assets/scripts/thanks_end.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _input(event):
+	if Input.is_anything_pressed():
+		%UI_Transition.play_backwards("Enter")
+		await %UI_Transition.animation_finished
+		get_tree().change_scene_to_file("res://assets/scenes/Main Menu.tscn")
